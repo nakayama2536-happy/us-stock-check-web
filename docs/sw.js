bc20574bf390083a3734be5efce01914f370d191
モゲパーマン <nakayama2536@gmail.com>
2026-09-22 07:14:27 +0900

    Bump PWA cache to v0.2.0

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE="us-stock-check-v0.1.1";
+const CACHE="us-stock-check-v0.2.0";
 const STATIC=["./","./index.html","./style.css","./app.js","./manifest.webmanifest","./icons/us-stock-icon.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
