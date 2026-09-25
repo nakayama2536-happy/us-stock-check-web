3ba0b6e071570c83fe8fb4e5286329ae064745ec
モゲパーマン <nakayama2536@gmail.com>
2026-09-26 08:00:19 +0900

    US web: refresh PWA cache for common UI

--- docs/sw.js
+++ docs/sw.js
@@ -1,9 +1,9 @@
-const CACHE="us-stock-check-v0.9.5-ops1";
+const CACHE="us-stock-check-v0.9.6-common1";
 const STATIC=[
   "./",
   "./index.html",
-  "./style.css?v=0.9.5-ops1",
-  "./app.js?v=0.9.5-ops1",
+  "./style.css?v=0.9.6-common1",
+  "./app.js?v=0.9.6-common1",
   "./manifest.webmanifest",
   "./icons/us-stock-icon.png"
 ];
